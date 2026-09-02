abc95017fd3f7d55c34114c184517126d01b1cf6
qiyuan <kongqiyuan31@gmail.com>
2026-09-02 22:37:29 +0800

    Tutorial-part2

---
+++ src/main/java/DialogBox.java
@@ -0,0 +1,22 @@
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+/**
+ * A reusable chat message containing text and an avatar.
+ */
+public class DialogBox extends HBox {
+    public DialogBox(String message, Image image) {
+        Label text = new Label(message);
+        ImageView displayPicture = new ImageView(image);
+
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        setAlignment(Pos.TOP_RIGHT);
+
+        getChildren().addAll(text, displayPicture);
+    }
+}
